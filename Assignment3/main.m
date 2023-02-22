c9074c605061d204f162073fa40e7e2214df13b6
LilKoke <k-hirano@wb3.so-net.ne.jp>
2023-02-22 13:58:31 +0900

    fix typo and grammar errors

--- Assignment3/main.m
+++ Assignment3/main.m
@@ -1,3 +1,5 @@
+nu = 1.327e11;
+
 fig_num = 1;
 t = [2023 1 1 0];
 for i=1:12
@@ -24,23 +26,23 @@ arrival_t = [2024 1 1 0];
 dt = arrival_t - launch_t;
 
 % r1, r2を求める
-[fig_num pos_list] = plot_planets(launch_t, fig_num);
+[fig_num, pos_list] = plot_planets(launch_t, fig_num);
 savefig(strcat('figure',num2str(fig_num),".fig"));
 fig_num = fig_num + 1;
 
 r1 = pos_list(1:3);
 
-[fig_num pos_list] = plot_planets(arrival_t, fig_num);
-r_2 = pos_list(7:9);
+[fig_num, pos_list] = plot_planets(arrival_t, fig_num);
+r2 = pos_list(7:9);
 savefig(strcat('figure',num2str(fig_num),".fig"));
 fig_num = fig_num + 1;
 
 % 遷移角を計算
-dnu = dot(r1, r2) / norm(r1) / norm(r2);
+dnu = acos(dot(r1, r2) / norm(r1) / norm(r2));
 
 % c, am, s, Tm, betamを計算
-c = sqrt(norm(r1)^2 + norm(r2)^2 - 2 * dot(r1, r2))
-am - (norm(r1) + norm(r2) + c) / 4;
+c = sqrt(norm(r1)^2 + norm(r2)^2 - 2 * dot(r1, r2));
+am = (norm(r1) + norm(r2) + c) / 4;
 s = 2 * am;
 % Tm = 2 * PI * sqrt(am^3 / r);
 betam = 2 * asin(sqrt((s - c) / s));
@@ -52,15 +54,16 @@ betam = 2 * asin(sqrt((s - c) / s));
 %     dtm = N * Tm + sqrt(am^3 / nu) * (pi + (betam - sin(betam)));
 
 % 放物線軌道での飛行時間を計算
-if 0 <= dnu && dnu <= 180
+if 0 <= dnu && dnu <= deg2rad(180)
     dtp = 1 / 3 * sqrt(2 / nu) * (s ^ (3/2) - (s - c)^(3/2));
-elseif 180 <= dnu && dnu <= 360
+elseif deg2rad(180) <= dnu && dnu <= deg2rad(360)
     dtpt = 1 / 3 * sqrt(2 / nu) * (s ^ (3/2) + (s - c)^(3/2));
 end
 
 if dt > dtp
-    orbit_type = "ellipse"
-elseif dt = dtp
-    orbit_type = "parabola"
-elseif
-    orbit_type = "hyperbola"
+    orbit_type = "ellipse";
+elseif dt == dtp
+    orbit_type = "parabola";
+else
+    orbit_type = "hyperbola";
+end
